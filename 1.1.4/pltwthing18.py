75a65ab4f95906fb97b611724794c7c3f818f5b7
Jason Zeng <101027720+IcedLimes@users.noreply.github.com>
2024-09-12 08:27:39 -0700

    Update pltwthing18.py

--- 1.1.4/pltwthing18.py
+++ 1.1.4/pltwthing18.py
@@ -1,51 +1,30 @@
+#   a114_nested_loops_4.py 
 import turtle as trtl
 
 painter = trtl.Turtle()
+painter.speed(0)
 painter.penup()
 painter.goto(-200, 0)
 painter.pendown()
 
-# First pattern
 x = -200
 y = 0
 move_x = 1
 move_y = 1
+# changed the 0 to a 1 
+while (x < 1):
 
-# Draw the first pattern (original)
-while (x < 0):
-    while (y < 100):
-        x = x + move_x
-        y = y + move_y
-        painter.goto(x,y)
-    move_y = -1
-    while (y > 0):
-        x = x + move_x
-        y = y + move_y
-        painter.goto(x,y)
-    move_y = 1
-
-# Set starting point for the opposite pattern
-x = 0
-y = 0
-painter.penup()
-painter.goto(x, y)
-painter.pendown()
-
-move_x = 1
-move_y = 1
-
-# Draw the opposite pattern
-while (x < 200):
-    while (y < 100):
-        x = x + move_x
-        y = y + move_y
-        painter.goto(x, y)
-    move_y = -1
-    while (y > 0):
-        x = x + move_x
-        y = y + move_y
-        painter.goto(x, y)
-    move_y = 1
+  while (y < 100):
+    x = x + move_x
+    y = y + move_y
+    painter.goto(x,y)
+  move_y = -1
+  
+  while (y > 0):
+    x = x + move_x
+    y = y + move_y
+    painter.goto(x,y)
+  move_y = 1
 
 wn = trtl.Screen()
 wn.mainloop()
