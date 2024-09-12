a514bd14a169755f7245ea56da3db60e2e50dbd0
Jason Zeng <101027720+IcedLimes@users.noreply.github.com>
2024-09-12 09:33:44 -0700

    Update pltwthing18.py

--- 1.1.4/pltwthing18.py
+++ 1.1.4/pltwthing18.py
@@ -25,5 +25,22 @@ while (x < 1):
     painter.goto(x,y)
   move_y = 1
 
+painter.penup()
+painter.goto(-200,0)
+painter.pendown()
+
+while (x < 1):
+
+  while (y > -100):
+    x = x + move_x
+    y = y - move_y
+    painter.goto(x,y)
+  
+  while (y < 0):
+    x = x + move_x
+    y = y + move_y
+    painter.goto(x,y)
+  
+
 wn = trtl.Screen()
 wn.mainloop()
